4c68a33371ad04e4949fa72fe38daab46695677b
LesterArana <laranam3@miumg.edu.gt>
2024-09-11 11:22:50 -0600

    Corrección vista login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="login-background" style="position: relative; height: 100vh; background-image: url('{{ asset('images/fondologin.jpg') }}'); background-size: cover; background-position: center; display: flex; align-items: center; justify-content: center;">
+    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
         <div class="col-md-6">
             <div class="card shadow-lg p-3 mb-5 bg-white rounded">
                 <div class="card-header bg-primary text-white text-center">
-                    <h4>{{ __('Login') }}</h4>
+                    <h4>{{ __('INSERPRO') }}</h4>
                 </div>
 
                 <div class="card-body">
@@ -14,7 +14,7 @@
                         @csrf
 
                         <div class="form-group mb-3">
-                            <label for="email" class="form-label">{{ __('Email Address') }}</label>
+                            <label for="email" class="form-label">{{ __('Correo electronico') }}</label>
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -24,7 +24,7 @@
                         </div>
 
                         <div class="form-group mb-3">
-                            <label for="password" class="form-label">{{ __('Password') }}</label>
+                            <label for="password" class="form-label">{{ __('Contraseña') }}</label>
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -37,21 +37,21 @@
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                 <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
+                                    {{ __('Recuerdame') }}
                                 </label>
                             </div>
                         </div>
 
                         <div class="d-grid gap-2 mb-3">
                             <button type="submit" class="btn btn-primary btn-block">
-                                <i class="fas fa-sign-in-alt"></i> {{ __('Login') }}
+                                <i class="fas fa-sign-in-alt"></i> {{ __('INICIA') }}
                             </button>
                         </div>
 
                         <div class="text-center">
                             @if (Route::has('password.request'))
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
+                                    {{ __('Olvidaste tu contraseña?') }}
                                 </a>
                             @endif
                         </div>
